6e6ee80094145bf36d6739c0a5d3cbbdfcfe58b5
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-06-11 14:42:05 -0400

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -4,23 +4,31 @@ const router = require('express').Router()
 
 const Project = require('./model')
 
-router.get('/', async (req, res, next) => {
-    try {
-        const projects = await Project.getAll()
-        res.json(projects)
-      } catch (err) {
-        next(err)
-      }
+router.get('/',  (req, res, next) => {
+  Project.getAll()
+  .then((projects) => {
+    projects.map(item => {
+      item.project_completed ? item.project_completed = true : item.project_completed = false
+            return item
+        })
+        res.status(200).json(projects);
+    })
+  .catch(err =>{
+    next(err)
+  })
 })
 
+
 router.post('/', async (req, res, next) => {
-    try {
-        const updated = await Project.create(req.body)
-        res.json(updated)
-      } catch (err) {
-        next(err)
-      }
-    });
+  Project.create(req.body)
+  .then(project=>{
+      project.project_completed ? project.project_completed = true : project.project_completed = false
+      res.status(201).json(project);
+  })
+  .catch(err=>{
+    next(err)
+  })
+})
 
 
 module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -4,22 +4,27 @@ const router = require('express').Router()
 const Task = require('./model')
 
 router.get('/', async (req, res, next) => {
-    try {
-        const tasks = await Task.getAll()
-        res.json(tasks)
-      } catch (err) {
-        next(err)
-      }
+  Task.getAll()
+  .then((tasks) => {
+    tasks.map(item => {
+      item.task_completed ? item.task_completed = true : item.task_completed = false
+            return item
+        })
+        res.status(200).json(tasks);
+    })
+  .catch(err =>{
+    next(err)
+  })
 })
 
 router.post('/', async (req, res, next) => {
-    try {
-        const updated = await Task.create(req.body)
-        res.json(updated)
-      } catch (err) {
-        next(err)
-      }
-    });
-
-
+  Task.create(req.body)
+  .then(task=>{
+      task.task_completed ? task.task_completed = true : task.task_completed = false
+      res.status(201).json(task);
+  })
+  .catch(err=>{
+    next(err)
+  })
+})
 module.exports = router
